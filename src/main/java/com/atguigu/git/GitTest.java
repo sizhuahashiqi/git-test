46440fdb5b253181b3d480a30f6a3726242f44da
kkk <874561465@qq.com>
2023-07-03 22:31:34 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello github!");
     }
 }
